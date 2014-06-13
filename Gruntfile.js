353f6702207f57c6d790528c52e677290b092430
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-06-13 20:47:11 +0300

    serve task updated

--- Gruntfile.js
+++ Gruntfile.js
@@ -309,7 +309,7 @@ module.exports = function (grunt) {
     }
   });
 
-  grunt.registerTask('server', function (target) {
+  grunt.registerTask('serve', function (target) {
     if (target === 'dist') {
       return grunt.task.run(['build', 'connect:dist:keepalive']);
     }
