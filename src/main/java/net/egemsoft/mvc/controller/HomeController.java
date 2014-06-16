e4f7af17230b3a1f15a3610a360112fada9b5172
İsmail Demirbilek <ce.demirbilek@gmail.com>
2014-06-16 15:17:15 +0300

    spring boot opted out. web app configurations added.

--- src/main/java/net/egemsoft/mvc/controller/HomeController.java
+++ src/main/java/net/egemsoft/mvc/controller/HomeController.java
@@ -1,13 +1,20 @@
+package net.egemsoft.mvc.controller;
+
+import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RequestMethod;
 
 /**
  * @author İsmail Demirbilek
  *         6/13/14
  */
+
+@Controller
+@RequestMapping("/")
 public class HomeController {
-    @RequestMapping("/")
+
+    @RequestMapping(method = RequestMethod.GET)
     public String home(Model model) {
         model.addAttribute("q", "Naber");
         return "index";
